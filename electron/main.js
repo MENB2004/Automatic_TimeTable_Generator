d9f79214d8bddd3aa19b0d4245c75096ecb0539c
Developer <developer@example.com>
2026-09-01 02:52:33 +0530

    Set Windows application icon (icon.ico) for executable and NSIS installer

--- electron/main.js
+++ electron/main.js
@@ -68,7 +68,7 @@ function createWindow() {
     minWidth: 900,
     minHeight: 600,
     title: 'ATG - Automatic Timetable Generator',
-    icon: path.join(__dirname, '..', 'assets', 'icon.png'),
+    icon: path.join(__dirname, '..', 'assets', 'icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
